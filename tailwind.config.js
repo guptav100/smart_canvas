913d1cb47669550704a73388d4de09ec48edd208
Chakit Gupta <76941922+chakitg@users.noreply.github.com>
2023-06-23 22:53:23 +0530

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-import withMT from "@material-tailwind/react/utils/withMT";
- 
-export default withMT({
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-});
